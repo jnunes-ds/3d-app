db4a14c37b783b1adcdfdce80d1f0dbc8f15a5ad
Júnior Nunes <denisonprobable13@gmail.com>
2021-07-25 20:19:35 -0300

    Cube on screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,61 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Expo from 'expo';
-import {Scene, Mesh, MeshBasicMaterial, PerspectiveCamera} from 'three';
+import {Scene, Mesh, MeshBasicMaterial, PerspectiveCamera, BoxBufferGeometry} from 'three';
 import ExpoTHREE, {THREE, Renderer} from 'expo-three';
-import {ExpoWebGLRenderingContext} from 'expo-gl';
+import {ExpoWebGLRenderingContext, GLView} from 'expo-gl';
+
+interface CanvasProps{
+  width: number;
+  height: number;
+}
+
+interface Props extends ExpoWebGLRenderingContext{
+  canvas: CanvasProps;
+}
 
 export default function App(){
+
+  async function onContextCreate(gl: Props){
+    //THREE.js code
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth/gl.drawingBufferHeight,
+      0.1,
+      1000
+    );
+
+    gl.canvas = {width: gl.drawingBufferWidth, height: gl.drawingBufferHeight}
+    camera.position.z = 2;
+
+    const renderer = new Renderer({gl});
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    const geometry =  new BoxBufferGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({
+      color: 'blue'
+    });
+    const cube = new Mesh(geometry, material);
+    scene.add(cube);
+
+    function render(){
+      requestAnimationFrame(render);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    }
+
+    render();
+  } 
   
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <GLView 
+        onContextCreate={onContextCreate}
+        style={styles.GLView}
+      />
     </View>
   );
 }
@@ -19,5 +65,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  GLView: {
+    width: 500,
+    height: 500
   }
 });
